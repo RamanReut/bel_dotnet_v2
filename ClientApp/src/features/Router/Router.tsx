24a50c5b2919b7b11b4df425fc3b0bda71e388f4
Raman Reut <r.reut@itransition.com>
2020-10-16 10:57:41 +0300

    Move EditNews into features/news

--- ClientApp/src/features/Router/Router.tsx
+++ ClientApp/src/features/Router/Router.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 const Main = React.lazy(() => import('../Main'));
-const EditNews = React.lazy(() => import('../EditNews'));
+const EditNews = React.lazy(() => import('../news/EditNews'));
 
 export default function Router(): React.ReactElement {
     return (

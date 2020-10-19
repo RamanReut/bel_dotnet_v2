4c2abcb704dad6dadee7bcfa5edb1eb094564d62
Raman Reut <r.reut@itransition.com>
2020-10-19 14:43:57 +0300

    Add SelectLanguage component

--- ClientApp/src/share/SelectLanguage/SelectLanguage.tsx
+++ ClientApp/src/share/SelectLanguage/SelectLanguage.tsx
@@ -1,6 +1,9 @@
 import React, { useCallback } from 'react';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { useTranslation } from 'react-i18next';
 
 export interface SelectLanguageProps {
     lang: string;
@@ -11,17 +14,43 @@ export default function SelectLanguage({
     lang,
     onChange,
 }: SelectLanguageProps): React.ReactElement {
+    const { t } = useTranslation();
+
     const handleChange = useCallback((event) => {
         onChange(event.target.value);
     }, [onChange]);
 
     return (
-        <Select
-            value={lang}
-            onChange={handleChange}
+        <Grid
+            container
+            alignItems="center"
         >
-            <MenuItem value="ru">Русский</MenuItem>
-            <MenuItem value="be">Белорусский</MenuItem>
-        </Select>
+            <Grid
+                item
+                sm={2}
+                xs={12}
+            >
+                <Typography
+                    variant="h6"
+                    align="left"
+                >
+                    {t('languageLabel')}
+                </Typography>
+            </Grid>
+            <Grid
+                item
+                sm={10}
+                xs={12}
+            >
+                <Select
+                    value={lang}
+                    onChange={handleChange}
+                    fullWidth
+                >
+                    <MenuItem value="ru">{t('languages.ru')}</MenuItem>
+                    <MenuItem value="be">{t('languages.be')}</MenuItem>
+                </Select>
+            </Grid>
+        </Grid>
     );
 }

514e69506b4547ba078a961ab05904df711fc2da
Raman Reut <r.reut@itransition.com>
2020-10-20 10:04:12 +0300

    Add News page

--- ClientApp/src/features/Router/Router.tsx
+++ ClientApp/src/features/Router/Router.tsx
@@ -3,10 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 
 const Main = React.lazy(() => import('../Main'));
 const EditNews = React.lazy(() => import('../news/EditNews'));
+const News = React.lazy(() => import('../news/News'));
 
 export default function Router(): React.ReactElement {
     return (
         <Switch>
+            <Route path="/news/:id">
+                <News />
+            </Route>
             <Route path="/editNews/:id">
                 <EditNews />
             </Route>

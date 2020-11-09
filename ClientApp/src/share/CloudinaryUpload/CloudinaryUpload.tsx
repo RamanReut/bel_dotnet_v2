873cb74a1995e3feda3e4eaaabc49d4e72193907
Raman Reut <r.reut@itransition.com>
2020-11-09 11:16:53 +0300

    Add image remove button and responsive for upload

--- ClientApp/src/share/CloudinaryUpload/CloudinaryUpload.tsx
+++ ClientApp/src/share/CloudinaryUpload/CloudinaryUpload.tsx
@@ -4,18 +4,107 @@ import React, {
     useCallback,
 } from 'react';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 import UploadIcon from '@material-ui/icons/CloudUpload';
+import DeleteIcon from '@material-ui/icons/Delete';
 import { makeStyles, Theme } from '@material-ui/core/styles';
 import ImageUploading, { ImageListType } from 'react-images-uploading';
 import { useTranslation } from 'react-i18next';
 import DragAccept from './DragAccept';
 
-const HEIGHT = '5rem';
+const HEIGHT = '6em';
+const REPONSIVE_HEIGHT_CHANGE = '6em';
 
-const useStyles = makeStyles((theme: Theme) => ({
+const useButtonStyle = makeStyles({
+    root: {
+        width: '10em',
+        justifyContent: 'flex-start',
+    },
+});
+
+interface ButtonWithIconProps {
+    children: string;
+    icon: React.ReactNode;
+    onClick?: ((event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void);
+}
+
+function ButtonWithIcon({
+    children,
+    icon,
+    onClick,
+}: ButtonWithIconProps) {
+    const classes = useButtonStyle();
+
+    return (
+        <Button
+            classes={classes}
+            startIcon={icon}
+            variant="contained"
+            color="primary"
+            onClick={onClick}
+        >
+            {children}
+        </Button>
+    );
+}
+
+interface ControlButtonGroupProps {
+    onUpload: () => void;
+    onRemove: () => void;
+    onUpdate: (index: number) => void;
+    isImageLoaded: boolean;
+}
+
+function ControlButtonGroup({
+    onUpload,
+    onRemove,
+    onUpdate,
+    isImageLoaded,
+}: ControlButtonGroupProps) {
+    const { t } = useTranslation();
+    const handleUpdate = useCallback(
+        () => {
+            onUpdate(0);
+        },
+        [onUpdate],
+    );
+
+    return (
+        <Grid
+            container
+            spacing={1}
+            direction="column"
+        >
+            <Grid item>
+                <ButtonWithIcon
+                    icon={<UploadIcon />}
+                    onClick={isImageLoaded ? () => handleUpdate : onUpload}
+                >
+                    {isImageLoaded ? t('update') : t('upload')}
+                </ButtonWithIcon>
+            </Grid>
+            {isImageLoaded
+                ? (
+                    <Grid item>
+                        <ButtonWithIcon
+                            icon={<DeleteIcon />}
+                            onClick={onRemove}
+                        >
+                            {t('remove')}
+                        </ButtonWithIcon>
+                    </Grid>
+                ) : <div />}
+        </Grid>
+    );
+}
+
+const useCloudinaryUploadStyles = makeStyles((theme: Theme) => ({
     root: {
         width: '100%',
         height: HEIGHT,
+        [theme.breakpoints.only('xs')]: {
+            height: `calc(${HEIGHT} + ${REPONSIVE_HEIGHT_CHANGE})`,
+        },
         position: 'relative',
     },
     dragging: {
@@ -24,29 +113,28 @@ const useStyles = makeStyles((theme: Theme) => ({
         borderStyle: 'dashed',
         borderWidth: '3px',
     },
-    buttonWrapper: {
+    controlWrapper: {
         position: 'relative',
         zIndex: 1,
         background: 'white',
         width: '100%',
         height: '100%',
         boxSizing: 'border-box',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        '&>*': {
-            margin: '0.25em',
+        margin: 0,
+        '&>.MuiGrid-item': {
+            paddingBottom: '0',
+            paddingTop: '0',
         },
     },
     image: {
-        height: '100%',
+        height: HEIGHT,
     },
 }));
 
 function CloudinaryUpload(): ReactElement {
     const [images, setImages] = useState<ImageListType>([]);
     const { t } = useTranslation();
-    const classes = useStyles();
+    const classes = useCloudinaryUploadStyles();
 
     const handleChange = useCallback((imageList) => {
         setImages(imageList);
@@ -60,12 +148,19 @@ function CloudinaryUpload(): ReactElement {
             {({
                 imageList,
                 onImageUpload,
+                onImageRemoveAll,
+                onImageUpdate,
                 dragProps,
                 isDragging,
             }) => (
                 <div className={classes.root}>
-                    <div
-                        className={classes.buttonWrapper}
+                    <Grid
+                        container
+                        direction="row"
+                        justify="center"
+                        alignItems="center"
+                        spacing={1}
+                        className={classes.controlWrapper}
                         onDragEnter={dragProps.onDragEnter}
                         onDragOver={dragProps.onDragOver}
                     >
@@ -74,25 +169,26 @@ function CloudinaryUpload(): ReactElement {
 
                             if (image) {
                                 return (
-                                    <img
-                                        className={classes.image}
-                                        src={image.dataURL}
-                                        alt={t('previewImageAlt')}
-                                    />
+                                    <Grid item>
+                                        <img
+                                            className={classes.image}
+                                            src={image.dataURL}
+                                            alt={t('previewImageAlt')}
+                                        />
+                                    </Grid>
                                 );
                             }
                             return <div />;
                         })()}
-                        <Button
-                            startIcon={<UploadIcon />}
-                            variant="contained"
-                            color="primary"
-                            onClick={onImageUpload}
-                            {...dragProps}
-                        >
-                            {t('upload')}
-                        </Button>
-                    </div>
+                        <Grid item>
+                            <ControlButtonGroup
+                                onUpload={onImageUpload}
+                                onRemove={onImageRemoveAll}
+                                onUpdate={onImageUpdate}
+                                isImageLoaded={!!images.length}
+                            />
+                        </Grid>
+                    </Grid>
                     <DragAccept
                         isDragging={isDragging as boolean}
                         {...dragProps}

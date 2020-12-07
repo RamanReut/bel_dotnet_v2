3061dc1b88cf61ec3a7d9d9114db5a8bb75daddf
Raman Reut <r.reut@itransition.com>
2020-12-07 09:04:35 +0300

    Add news list page into routing

--- ClientApp/src/features/Router/Router.tsx
+++ ClientApp/src/features/Router/Router.tsx
@@ -4,10 +4,14 @@ import { Route, Switch } from 'react-router-dom';
 const Main = React.lazy(() => import('../Main'));
 const EditNews = React.lazy(() => import('../news/EditNews'));
 const News = React.lazy(() => import('../news/News'));
+const NewsList = React.lazy(() => import('../news/NewsList'));
 
 export default function Router(): React.ReactElement {
     return (
         <Switch>
+            <Route path="/news/all">
+                <NewsList />
+            </Route>
             <Route path="/news/:id">
                 <News />
             </Route>
---
+++ ClientApp/src/features/news/NewsList/NewsList.tsx
@@ -0,0 +1,7 @@
+import React, { ReactElement } from 'react';
+
+function NewsList(): ReactElement {
+    return <div />;
+}
+
+export default NewsList;

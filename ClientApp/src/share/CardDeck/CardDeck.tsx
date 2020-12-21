c51d2b2f08f9642d8d3cf08811eb7618f9fd4e4c
Raman Reut <r.reut@itransition.com>
2020-12-21 22:26:08 +0300

    Implement CardDeck component

--- ClientApp/src/share/CardDeck/CardDeck.tsx
+++ ClientApp/src/share/CardDeck/CardDeck.tsx
@@ -1,7 +1,58 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, ReactChild } from 'react';
+import Pagination from '@material-ui/lab/Pagination';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 
-function CardDeck(): ReactElement {
-    return <div />;
+const useStyles = makeStyles({
+    root: {
+        '& > *': {
+            paddingBottom: '1em',
+        },
+    },
+    ul: {
+        display: 'inline-flex',
+    },
+    empty: {
+        padding: 0,
+    },
+});
+
+export interface CardDeckProps{
+    cards?: ReactChild[];
+    pageCount?: number;
+}
+
+function CardDeck({
+    cards = [],
+    pageCount = 0,
+}: CardDeckProps): ReactElement {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <Grid container>
+                {cards.map((cur, index) => (
+                    <Grid
+                        item
+                        key={index.toString()}
+                    >
+                        {cur}
+                    </Grid>
+                ))}
+            </Grid>
+            {
+                pageCount !== 0
+                    ? (
+                        <Pagination
+                            classes={{ ul: classes.ul }}
+                            count={pageCount}
+                        />
+                    )
+                    : <div className={classes.empty} />
+            }
+
+        </div>
+    );
 }
 
 export default CardDeck;

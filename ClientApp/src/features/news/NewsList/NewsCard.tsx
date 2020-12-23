991ac46de3138cfe459a249e9583b89271b70994
Raman Reut <r.reut@itransition.com>
2020-12-23 21:08:47 +0300

    Add NewsList and NewsCard components

--- ClientApp/src/features/news/NewsList/NewsCard.tsx
+++ ClientApp/src/features/news/NewsList/NewsCard.tsx
@@ -1,7 +1,66 @@
 import React, { ReactElement } from 'react';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardHeader from '@material-ui/core/CardHeader';
+import { makeStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
+import { Image } from 'cloudinary-react';
 
-function NewsCard(): ReactElement {
-    return <div />;
+const IMAGE_WIDTH = 240;
+const ASPECT_RATIO = 1.6;
+
+const useStyles = makeStyles(
+    (theme) => ({
+        root: {
+            width: `${IMAGE_WIDTH}px`,
+            '& a': {
+                textDecoration: 'none',
+                color: theme.palette.text.primary,
+                '&:hover': {
+                    textDecoration: 'underline',
+                },
+                '&:visited': {
+                    color: theme.palette.text.primary,
+                },
+            },
+        },
+    }),
+);
+
+export interface NewsCardProps {
+    id: number;
+    title: string;
+    image: string;
+}
+
+function NewsCard({
+    id,
+    title,
+    image,
+}: NewsCardProps): ReactElement {
+    const classes = useStyles();
+
+    return (
+        <Card
+            elevation={5}
+            className={classes.root}
+        >
+            <Link to={`/news/${id}`}>
+                <CardMedia>
+                    <Image
+                        responsive
+                        publicId={image}
+                        width="auto"
+                        aspectRatio={ASPECT_RATIO}
+                        crop="fill"
+                    />
+                </CardMedia>
+                <CardHeader
+                    title={title}
+                />
+            </Link>
+        </Card>
+    );
 }
 
 export default NewsCard;
--- ClientApp/src/features/news/NewsList/NewsList.tsx
+++ ClientApp/src/features/news/NewsList/NewsList.tsx
@@ -1,10 +1,79 @@
-import React, { ReactElement } from 'react';
+import React, {
+    ReactElement,
+    useEffect,
+} from 'react';
+import { useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
+import { makeStyles } from '@material-ui/core';
+import { useAppDispatch } from '../../store';
 import Section from '../../../share/Section';
+import CardDeck from '../../../share/CardDeck';
+import { getTranslation } from '../../../share/translationContainer';
+import NewsCard from './NewsCard';
+import {
+    actions,
+    selectors,
+} from '../reducer';
+
+const NEWS_PER_PAGE = 9;
+
+const useStyles = makeStyles(
+    (theme) => ({
+        root: {
+            backgroundColor: theme.palette.grey[100],
+        },
+    }),
+);
 
 function NewsList(): ReactElement {
+    const dispatch = useAppDispatch();
+    const { i18n } = useTranslation();
+    const classes = useStyles();
+
+    const page = useSelector(selectors.list.page);
+    const newsCount = useSelector(selectors.data.maxCount);
+
+    const start = (page - 1) * NEWS_PER_PAGE;
+    const end = page * NEWS_PER_PAGE;
+    const news = useSelector(selectors.data.newsList(start, end));
+
+    useEffect(
+        () => {
+            dispatch(actions.data.initNewsCount());
+        },
+        [dispatch],
+    );
+
+    useEffect(
+        () => {
+            dispatch(
+                actions.data.initShortNewsList({ start, end }),
+            );
+        },
+        [dispatch, end, page, start],
+    );
+
     return (
-        <Section>
-            <div />
+        <Section
+            className={classes.root}
+        >
+            <CardDeck
+                page={page}
+                pageCount={Math.ceil(newsCount / NEWS_PER_PAGE)}
+            >
+                {news.map((cur) => {
+                    const title = getTranslation(cur.title, i18n.language);
+
+                    return (
+                        <NewsCard
+                            key={cur.id}
+                            id={cur.id}
+                            title={title}
+                            image={cur.previewImage}
+                        />
+                    );
+                })}
+            </CardDeck>
         </Section>
     );
 }
